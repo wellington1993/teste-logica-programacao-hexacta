d563dc5ab1212876f4db9b611a5004e9fd835f40
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2022-04-12 08:51:40 -0300

    TODO: Exercicios
    Signed-off-by: Wellington Torrejais da Silva <wtds.trabalho@gmail.com>

---
+++ solution_three.rb
@@ -0,0 +1,17 @@
+# você pode usar os comandos puts ou print para fins de debug, ex.
+# print "$var definido\n" if $var
+
+def solution3(m, p)
+  # escreva o seu código Ruby
+end
+
+=begin
+de forma que o método solution3(p, m), ao receber um inteiro m (sendo 1 <= m <= 10^9) e um valor float
+p, retorne o valor de p elevado a m (ou p ** m) sem utilizar bibliotecas matemáticas ou a operação de
+exponenciação nativa do Ruby e que seja computacionalmente eficiente. Por exemplo,
+• solution3 0.999999999999, 0 = 1.0
+• solution3 0.999999999999, 1000 = 0.9999999990000221
+• solution3 0.999999999999, 1000000 = 0.9999990000226197
+• solution3 0.999999999999, 1000000000 = 0.9990005219311262
+=end
+
---
+++ solution_two.rb
@@ -0,0 +1,10 @@
+
+
+=begin
+Escreva um método Ruby que receba um array de números ordenados de forma ascendente e
+uma variável numérica x e calcule, da forma mais eficiente possível, todas as combinações de pares de
+números deste array cuja a soma é igual a x.
+Exemplo: Dado um array = [-2, -1, 0, 2, 4, 7, 8, 9, 9] e x = 8 como entrada, a resposta do seu algoritmo deverá
+ser um array [[-1, 9], [0, 8], [4, 4]] (não precisando ser necessariamente nesta ordem)
+=end
+
