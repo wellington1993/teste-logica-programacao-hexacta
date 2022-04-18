511b026cb403f3ce3213b7417801b4b2721ef6ed
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2022-04-18 05:04:10 -0300

    Update solution_two.rb

--- solution_two.rb
+++ solution_two.rb
@@ -1,15 +1,17 @@
-def find_somas(arr, k)
+def solution_two(arr, x)
   checked = Set.new  
   arr = arr.sort.uniq
   
   arr.each do |a|
     arr.each do |b|
-      checked.add([a,b]) if ( (a+b) == k )
+      checked.add([a,b]) if ( (a+b) == x )
     end
   end
-  
+  puts checked
   checked
 end
+
+solution_two([-2, -1, 0, 2, 4, 7, 8, 9, 9], 8)
 
 =begin
 Escreva um método Ruby que receba um array de números ordenados de forma ascendente e
